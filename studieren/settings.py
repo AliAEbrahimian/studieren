3973324518070a0cc37376267df1af78ec492fe2
Ali <2aaghaebrahimian@gmail.com>
2025-08-16 23:43:18 +0330

    creat Apps

--- studieren/settings.py
+++ studieren/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'base.apps.BaseConfig',
+    'cart.apps.CartConfig',
+    'products.apps.ProductsConfig'
+    'user.apps.UserConfig'
+
 ]
 
 MIDDLEWARE = [

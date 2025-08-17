eb291a4f7960d41d58af896999895e4ec1bb3db5
Ali <2aaghaebrahimian@gmail.com>
2025-08-17 18:49:15 +0330

    #

--- base/views.py
+++ base/views.py
@@ -2,15 +2,21 @@ from django.shortcuts import render
 
 # Create your views here.
 rooms = [
-    {'id': 1, 'name': 'Kurs 1'},
-    {'id': 2, 'name': 'Kurs 2'},
-    {'id': 3, 'name': 'Kurs 3'},
-    {'id': 4, 'name': 'Kurs 4'},
+    {'id' : 1, 'name' : 'Kurs 1'},
+    {'id' : 2, 'name' : 'Kurs 2'},
+    {'id' : 3, 'name' : 'Kurs 3'},
+    {'id' : 4, 'name' : 'Kurs 4'},
 ]
 
 def home(request):
-    context= {'rooms': rooms}
+    context = {'rooms': rooms}
     return render(request, 'base/home.html' , context)
 
-def room(request):
-    return render(request,'room.html')
+def room(request, pk):
+    room = None
+    for i in rooms :
+        if i['id'] == int(pk):
+            room = i
+    context = {'room' : room}
+    return render(request,'base/room.html', context)
+
